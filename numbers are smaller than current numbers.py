388ac629f5f66149a9b90d79f92c37aeb1ed0175
bhashakhan777 <bhashakhanpatannagur@gmail.com>
2024-12-21 09:55:35 +0530

    Delete numbers are smaller than current numbers.py

--- numbers are smaller than current numbers.py
+++
@@ -1,9 +0,0 @@
-def smallerNumbersThanCurrent(nums):
-    result = []
-    for i in range(len(nums)):
-        count = 0
-        for j in range(len(nums)):
-            if nums[j] < nums[i] and j != i:
-                count += 1
-        result.append(count)
-    return result
